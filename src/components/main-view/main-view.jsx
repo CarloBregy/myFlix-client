a2d869e20873bb69b5004aa03d0f7360905a22fe
CB <carlo_bregy@outlook.com>
2025-09-22 15:34:25 +0200

    Added Movie Infos

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,47 +6,86 @@ export const MainView = () => {
   const [movies, setMovies] = useState([
     {
       id: 1,
-      title: "Eloquent JavaScript",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg",
-      author: "Marijn Haverbeke"
+      Title: "The Dark Knight",
+      Description:
+        "Batman faces the Joker, a criminal mastermind who seeks to create chaos in Gotham City.",
+      Genre: {
+        Name: "Action",
+        Description:
+          "Action films are characterized by physical feats, fights, chases, and explosions.",
+      },
+      Director: {
+        Name: "Christopher Nolan",
+        Bio: "Updated. Christopher Edward Nolan is a British-American film director, producer, and screenwriter known for his innovative storytelling and cinematic style.",
+        Birth: "1970",
+      },
+      Year: 2008,
+      Rating: 9,
+      ImagePath: "thedarkknight.png",
+      Featured: true,
     },
     {
       id: 2,
-      title: "Mastering JavaScript Functional Programming",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/51WAikRq37L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-      author: "Federico Kereki"
+      Title: "Se7en",
+      Description:
+        "Two detectives hunt a serial killer who uses the seven deadly sins.",
+      Genre: {
+        Name: "Thriller",
+        Description:
+          "Thriller films involve suspense, excitement, and tension to keep the audience engaged.",
+      },
+      Director: {
+        Name: "David Fincher",
+        Bio: "David Andrew Leo Fincher is an American director known for his dark and stylish films.",
+        Birth: "1962",
+      },
+      Year: 1995,
+      Rating: 8.6,
+      ImagePath: "se7en.png",
+      Featured: false,
     },
     {
       id: 3,
-      title: "JavaScript: The Good Parts",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
-      author: "Douglas Crockford"
-    },
+      Title: "Pulp Fiction",
+      Description:
+        "The lives of two mob hitmen, a boxer, and a pair of diner bandits intertwine in tales of violence and redemption.",
+      Genre: {
+        Name: "Crime",
+        Description:
+          "Crime films focus on criminals, their motives, and law enforcement pursuing them.",
+      },
+      Director: {
+        Name: "Quentin Tarantino",
+        Bio: "Quentin Jerome Tarantino is an American filmmaker known for his stylized films and sharp dialogue.",
+        Birth: "1963",
+      },
+      Year: 1994,
+      Rating: 8.9,
+      ImagePath: "pulpfiction.png",
+      Featured: true,
+    }
   ]);
 
-  const [selectedBook, setSelectedBook] = useState(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-  if (selectedBook) {
+  if (selectedMoives) {
     return (
-      <BookView book={selectedBook} onBackClick={() => setSelectedBook(null)} />
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
     );
   }
 
-  if (books.length === 0) {
+  if (movies.length === 0) {
     return <div>The list is empty!</div>;
   }
 
   return (
     <div>
-      {books.map((book) => (
-        <BookCard
-          key={book.id}
-          book={book}
-          onBookClick={(newSelectedBook) => {
-            setSelectedBook(newSelectedBook);
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
           }}
         />
       ))}
